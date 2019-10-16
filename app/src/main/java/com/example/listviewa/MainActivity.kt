c5a0fda0eef0b2eb8d7ffb08b34134b974e6d7c5
unknown <fatiha.razy@student.uty.ac.id>
2019-10-17 00:18:19 +0700

    ListView

---
+++ app/src/main/java/com/example/listviewa/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.listviewa
+
+import android.R.layout.simple_list_item_1
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+            //data yang akan ditampilkan ke dalam ListVie
+        val languages = listOf ("Kotlin", "Pyton", "Ruby", "JavaScript", "PHP")
+        // memberikan adapter ke ListView
+        list_view.adapter = ArrayAdapter (this, simple_list_item_1, languages)
+        //menampilkan item saatdiclick
+
+        list_view.setOnItemClickListener {parent, view, position, id -> Toast.makeText(this, "Anda Memilih:${languages[position]}",Toast.LENGTH_SHORT).show()}
+
+    }
+}
